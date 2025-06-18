6d6e6f278fbc8c9fb24a15f00fa1ef2c43cc4c9b
PauetGG <paugallardogiralt@gmail.com>
2025-06-18 12:16:02 +0200

    Backend inicializado

---
+++ NutriODS-backend/src/main/java/com/nutri/NutriOdsApplication.java
@@ -0,0 +1,13 @@
+package com.nutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriOdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriOdsApplication.class, args);
+	}
+
+}
---
+++ NutriODS-backend/src/test/java/com/nutri/NutriOdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriOdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
